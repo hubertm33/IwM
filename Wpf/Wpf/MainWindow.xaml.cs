22290b087e78c995d623b9fab92e20ae49a032c3
hubertm33 <hubertm33@wp.pl>
2017-04-14 17:04:45 +0200

    cos jest. wyrazenie regularne na poprawne zdanie potrzebne

--- Wpf/Wpf/MainWindow.xaml.cs
+++ Wpf/Wpf/MainWindow.xaml.cs
@@ -20,9 +20,58 @@ namespace Wpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<string> stopWords;
+
         public MainWindow()
         {
+            stopWords = new List<string>();
+            String line;
+            
+            System.IO.StreamReader file =
+               new System.IO.StreamReader("stop_words.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                stopWords.Add(line);
+                
+            }
+            stopWords.ToArray();
+            file.Close();
+
             InitializeComponent();
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            Query.Clear();
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            //wyrazenie regularne na poprawne zdanie
+
+            String[] textArray = Query.Text.Split(' ', ',', ':', ';');
+            
+            List<string> textList = new List<string>();
+
+            foreach (string item in textArray)
+            {
+                textList.Add(item);
+            }
+            //textArray.ToList(); nie dziala
+            for (int i = 0;i < textArray.Length;i++)
+            {
+                foreach(String stopWord in stopWords)
+                {
+                    
+                    if (String.Equals(textArray[i], stopWord))
+                    {
+                        textList.Remove(textArray[i]);
+                        
+                    }
+                }
+            }
+
+
         }
     }
 }
